dda8e5902c48461f847474d88088ff60a11ddf22
Saran Pariyar <spariyar05@gmail.com>
2024-06-17 22:40:59 +0545

    JS thread

---
+++ basic-js/thread_and_call_stack.js
@@ -0,0 +1,11 @@
+/**
+ * THREAD OF EXECUTION
+ * 
+ * JS is a single-threaded language. 
+ * Single-thread means, there is single thread where task will be done one by one sequentially
+ * That means JS finish one task and then move to another task.
+ * JS Synchronous language with asynchronous capabilities. 
+ * A thread has a call stack & memory
+ */
+
+// Now learn call-stack
