2240a57b24bd58c91d9409df6f87ce5b2a846364
Saran Pariyar <spariyar05@gmail.com>
2024-07-07 14:33:04 +0545

    more on async and callstack

--- basic-js/async_and_event_loop.js
+++ basic-js/async_and_event_loop.js
@@ -14,12 +14,27 @@
  /**
 Event loop & Callback queue
 
+Callback func only run when call stack is clear, that means if the code takes 10 second, callback func will run after 10s even if it's only 2second delay in setTimeOut
+
+
 If we create setTimeOut, the callback function is registered in JS runtime env and the timer is running.
 
 After the time expires, it puts that function into CallBack Queue.
 
+
 Now the Event loop will check the callback function in CallBack Queue and put it into call stack where it will be executed
 
 Event loop acts like a gatekeeper. It's only work is to continuously monitor Call stack and event queue so executed the callback function one by one. It check first if the call stack is empty and then execute the callback function
+
+There is also a microtask queue just like callback queue  
+
+The event loop prioritizes the microtask queue (which includes promises and fetch callbacks) over the macro task queue (which includes setTimeout), so microtasks execute faster than macrotasks.
+
+
+
+
+
+
+
   
   */
